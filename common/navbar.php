57f568606c4064f969043e77bb16e805a96bf592
raganikushwaha <raganikushwaha1712@gmail.com>
2023-12-07 11:42:50 +0530

    setup basic 1

---
+++ common/navbar.php
@@ -0,0 +1,13 @@
+<?php 
+    require_once 'config.php';
+?>
+
+
+<ul>
+    <li>
+        <a href="<?= BASE_URL ?>index.php">home</a>
+    </li>
+    <li>
+        <a href="<?= BASE_URL ?>test/test.php">test</a>
+    </li>
+</ul>
