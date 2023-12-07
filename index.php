003b5ad6ce6bd8da4ef18ff928a7e4f0a07e01b5
raganikushwaha <raganikushwaha1712@gmail.com>
2023-12-08 00:59:03 +0530

    add_navbar

--- index.php
+++ index.php
@@ -7,18 +7,86 @@
 
 		<!-- Bootstrap CSS -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href ="asset/css/style.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
 
 		<title>MediHost</title>
   	</head>
 
+
   	<body>
-    	<h1>Hello, world!</h1>
+	    <!--  -->
+		<header>
+			<nav class="navbar navbar-expand-lg navigation-wrap">
+				<div class="container">
+					<a class="navbar-brand" href="#"><img src="images/logo.jpg"></a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+					aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+					<i class="fas fa-stream navbar-toggler-icon"></i></button>
+					<div class="collapse navbar-collapse" id="navbarText">
+						<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+							<li class="nav-item"><a class="nav-link active" aria-current="page" href="#home">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="#about">About Us</a></li>
+							<li class="nav-item"><a class="nav-link" href="#about">About </a></li>
+							<li class="nav-item"><a class="nav-link" href="#about">video</a></li>
+							<li class="nav-item"><a class="nav-link" href="#about">faq</a></li>					 
+						</ul>
+						<button class="main-btn">Contact</button>
+					</div>
+				</div>
+			</nav>
+		</header>
 
+		<!-- section-1 top-banner -->
+			 
+		<section> 
+			<div class="topbanner"></div>
+			<div class="topbannercontent">
+				<h1 class="text-white">We are ready <br>to help health <br> problem.</h1>
+				<p class="text-white mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit.  <br>Etiam et purus a odio  finibus bibendum amet leo.</p>
+				<div class="mt-5">
+					<button class="banner-btn">Get free consultation   <i class="fa-solid fa-arrow-left ps-2"></i>  </button>
+				</div>
+		    </div>  	 
+		</section>
+        
 
+		<!-- about us -->
+		<section>
+			<div class="container-fluid bg-light">
+				<div class="row">
+					<div class="col-md-6">
+						<img src="../img/banner.jpg">
+						<img src="../img/banner.jpg">
+					</div>
+					<div class="col-md-6">
+						<h6>About us</h6>
+						<h1>The Great place of medical hospital center.</h1>
+						<p >Lorem ipsum dolor sit amet, consectetur adipiscing elit.  <br>Etiam et purus a odio  finibus bibendum amet leo.</p>
+						<ul class="list-unstyled py-3">    
+							<li>phramacy on clinic</li>
+							<li>phramacy on clinic</li>
+							<li>phramacy on clinic</li>
+							<span><li>phramacy on clinic</li></span>
+							<li>phramacy on clinic</li></span>
+							 
+						</ul> 
+						<button class=" ">Learn More</button>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+   
+		 
+     
+		
 
-
-
-
+ 
+	 
+	 
+	
+		 
 
 
 
