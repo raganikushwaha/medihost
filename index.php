2caa780638e5f4dbb2733cf47c271c59eec5271c
raganikushwaha <raganikushwaha1712@gmail.com>
2023-12-08 17:21:03 +0530

    add new sections

--- index.php
+++ index.php
@@ -27,9 +27,9 @@
 						<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 							<li class="nav-item"><a class="nav-link active" aria-current="page" href="#home">Home</a></li>
 							<li class="nav-item"><a class="nav-link" href="#about">About Us</a></li>
-							<li class="nav-item"><a class="nav-link" href="#about">About </a></li>
-							<li class="nav-item"><a class="nav-link" href="#about">video</a></li>
-							<li class="nav-item"><a class="nav-link" href="#about">faq</a></li>					 
+							<li class="nav-item"><a class="nav-link" href="#doctors">Our Doctors</a></li>
+							<li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+							<li class="nav-item"><a class="nav-link" href="#about">Hospitals</a></li>					 
 						</ul>
 						<button class="main-btn">Contact</button>
 					</div>
@@ -39,7 +39,7 @@
 
 		<!-- section-1 top-banner -->
 			 
-		<section> 
+		<section id="home"> 
 			<div class="topbanner"></div>
 			<div class="topbannercontent">
 				<h1 class="text-white">We are ready <br>to help health <br> problem.</h1>
@@ -49,33 +49,174 @@
 				</div>
 		    </div>  	 
 		</section>
+
+		<!-- feature section -->
+		<section></section>
         
 
 		<!-- about us -->
-		<section>
-			<div class="container-fluid bg-light">
+		<section id="about">
+			<div class="container-fluid aboutbg">
+				<div class="container">
 				<div class="row">
-					<div class="col-md-6">
-						<img src="../img/banner.jpg">
-						<img src="../img/banner.jpg">
+					<div class="col-md-6 col-sm-6">
+						<div class="aboutimg">
+							<img class="hero1" src="asset/img/banner.jpg" style="width:350px; height:400px;">
+							<img class="hero2" src="asset/img/hero-fullscreen-bg.jpg" style="width:350px; height:400px;">
+						</div>
 					</div>
-					<div class="col-md-6">
-						<h6>About us</h6>
-						<h1>The Great place of medical hospital center.</h1>
-						<p >Lorem ipsum dolor sit amet, consectetur adipiscing elit.  <br>Etiam et purus a odio  finibus bibendum amet leo.</p>
-						<ul class="list-unstyled py-3">    
-							<li>phramacy on clinic</li>
-							<li>phramacy on clinic</li>
-							<li>phramacy on clinic</li>
-							<span><li>phramacy on clinic</li></span>
-							<li>phramacy on clinic</li></span>
-							 
-						</ul> 
-						<button class=" ">Learn More</button>
+					<div class="col-md-6 col-sm-6">
+						<div class="about">
+							<h6>About us</h6>
+							<h1>The Great place of medical hospital center.</h1>
+							<p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio  finibus bibendum amet leo.</p>
+							<div class="check">
+								<ul class="">    
+									<li>phramacy on clinic</li>
+									<li>phramacy on clinic</li>
+									<li>phramacy on clinic</li>
+									<li>phramacy on clinic</li> 
+									<li>phramacy on clinic</li>							
+								</ul> 
+							</div>
+							<button class="aboutbtn">Discover More</button>
+						</div>											
+					</div>
+				</div>
+				</div>
+			</div>		 
+		</section>
+		 
+
+		<!-- section-3 story-->
+		<section id="doctors">
+			<div class="story-section">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 col-md-12 mt-5 mb-5">
+							<div class="text-content">
+							<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+							<h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio
+								finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</h6>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio
+								finibus bibendum in sit amet leo. Mauris feugiat erat tellus.
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio
+								finibus bibendum in sit amet leo. Mauris feugiat erat tellus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio
+								finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
+								<button class="storybtn mt-3">Discover More</button>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</section>
+        </section>
+
+		<!-- services -->
+		 
+		<section id="services">
+			<div class="container mb-5">
+				<div class="services">
+					<div class="row">
+						<div class="col-md-12 text-center mt-5"><h6>Services</h6></div>
+						<div class="col-sm-12">
+							<div class="text-content text-center">
+								<h2>Lorem ipsum dolor sit amet.</h2>
+								<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit
+								amet leo. Mauris feugiat erat tellus. Far far away, behind the word mountains, far from the countries
+								Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+							</div>
+						</div>
+					</div>
+					
+					<div class="row mt-5">
+						<div class="col-md-3 mb-5">
+							<div class="card">
+								<div class="cardesign">
+									<i class="fa fa-users fa-2x circle-icon" style="color:#ffff";></i>
+									<div class="pt-3">
+										<h5>Lorem ipsum </h5>
+										<p class="mt-5 text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus 
+										amet leo.  </p>          
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-3 mb-5">
+							<div class="card">
+								<div class="cardesign">
+									<i class="fa fa-users fa-2x circle-icon" style="color:#ffff";></i>
+									<div class="pt-3">
+										<h5>Lorem ipsum </h5>
+										<p class="mt-5 text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus 
+										amet leo.  </p>          
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 mb-5">
+							<div class="card">
+								<div class="cardesign">
+									<i class="fa fa-users fa-2x circle-icon" style="color:#ffff";></i>
+									<div class="pt-3">
+										<h5>Lorem ipsum </h5>
+										<p class="mt-5 text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus 
+										amet leo.  </p>          
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 mb-5">
+							<div class="card">
+								<div class="cardesign">
+									<i class="fa fa-users fa-2x circle-icon" style="color:#ffff";></i>
+									<div class="pt-3">
+										<h5>Lorem ipsum </h5>
+										<p class="mt-5 text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus 
+										amet leo.  </p>          
+									</div>
+								</div>
+							</div>
+						</div>					
+				    </div>
+				</div>			
+			</div>
+        </section>
+
+
+		<!-- section-8 newslettar-->
+		<section id="newslettar">
+		    <div class="container-fluid newslettar">
+				<div class="row">
+					<div class="newsleter">
+					<div class="col-md-12 text-center mt-5"><h6>Newsletter</h6></div>
+					<div class="sol-sm-12">
+						<div class="text-content text-center pb-4">
+							<h2>JOIN US</h2>
+							<p> Lorem, ipsum dolor sit amet molestias aperiam quo accusamus fuga ducimus. </p>
+						</div>
+						<form class="newsletter">
+						<div class="row">
+							<div class="col-md-12 mt-4">
+							    <input class="form-control" placeholder="Email Address here" name="email" type="email">					 
+							    <button class="subscribe-btn" type="submit">Subscribe</button>
+							</div>
+							 
+						</form>
+						</div>
+					</div>
+				</div>
+			</div>			
+        </section>    
+
+		
+ 
+
+   
+
+  
+
+		 <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 		
    
 		 
